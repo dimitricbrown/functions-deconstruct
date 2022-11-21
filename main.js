3c85ce8430812acb8c2c228ccca6177214447f9b
dimitricbrown <dimitricbrown@outlook.com>
2022-11-21 09:19:42 -0600

    Added final code

---
+++ main.js
@@ -0,0 +1,35 @@
+const getDressed = () => {
+	console.log("Let's pick out our best workout attire! Make sure you look good and wear comfortable and flexible shoes")
+}
+
+const setPlaylist = () => {
+	console.log("Find some a combination of fun and chill/sensual songs.")
+}
+
+const findLocation = () => {
+	console.log("Look for a space with a clear floor and plenty of mirrors to see yourself.")
+}
+
+const gatherMaterials = () => {
+	console.log("Make sure to grab your mirror phone holder, earbuds or speaker, phone charger and water bottle! Maybe even a towel, cause you will be sweating!")
+}
+
+const travelToSpot = () => {
+	console.log("Travel to your destination, even if it's at home!")
+}
+
+const setUp = () => {
+	console.log("Get that phone holder placed, phone set to record, playlist ready and make sure you don't need to use the restroom.")
+}
+
+const jamSesh = () => {
+	console.log("Now get ready to hit play/record and show off your best dancing!")
+}
+
+getDressed()
+setPlaylist()
+findLocation()
+gatherMaterials()
+travelToSpot()
+setUp()
+jamSesh()
